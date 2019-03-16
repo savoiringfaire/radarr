54d343d829c693ba67ca36632e2ffb9dad6b22f7
Marcus <marcus@microserve.io>
2019-03-16 11:29:30 +0000

    Add some initial code

---
+++ manifests/init.pp
@@ -0,0 +1,31 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include radarr
+class radarr {
+  apt::ppa { 'ppa:marcushann/radarr': }
+
+  package { 'radarr': }
+
+  user { 'radarr':
+    ensure => present
+  }
+
+  file { '/home/radarr':
+    ensure => directory,
+    owner => 'radarr',
+    group => 'radarr'
+  }
+
+  file { '/lib/systemd/system/radarr.service':
+    ensure => file,
+    content => template('radarr/radarr.service.erb')
+  }
+
+  service { 'radarr':
+    ensure => running,
+    enable => true,
+  }
+}
